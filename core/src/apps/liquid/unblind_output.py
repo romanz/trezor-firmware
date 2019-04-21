582fe5258b1e40428fbcf1fbc31bd1a6b3e0a131
Roman Zeyde <me@romanzey.de>
2019-04-21 17:48:44 +0300

    Add Liquid support
    Re-generated messages using:
    $ (cd ../tools; ./build_protobuf)
    $ (cd python; pip3 install --user -e .)
    
    Currently supports blinding and unblinding an output (w/ Pedersen commitments and rangeproofs).
    Surjection proofs are supported only for n_inputs <= 16 (requires secp256k1-zkp patch).
    
    TODOs:
    * Randomness is passed as arguments.

---
+++ core/src/apps/liquid/unblind_output.py
@@ -0,0 +1,11 @@
+from trezor.crypto.curve import secp256k1_zkp
+from trezor.crypto.hashlib import sha256
+from trezor.messages.LiquidAmount import LiquidAmount
+
+from . import blind
+
+
+async def unblind_output(ctx, msg, keychain):
+    return blind.unblind_output(blinded=msg.blinded,
+                          ecdh_privkey=msg.ecdh_privkey,
+                          committed_script=msg.committed_script)
