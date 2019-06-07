13402520376f1702366889f336773712a3802ea9
Roman Zeyde <me@romanzey.de>
2019-06-07 12:43:55 +0300

    Use internal key derivation for unblinding external txns

--- core/src/apps/liquid/__init__.py
+++ core/src/apps/liquid/__init__.py
@@ -5,6 +5,6 @@ from apps.common import HARDENED
 
 
 def boot():
-    ns = [["secp256k1", HARDENED | 44, HARDENED | 1776]]
+    ns = [["secp256k1", HARDENED | 77]]
     wire.add(MessageType.LiquidBlindTx, __name__, "blind_tx", ns)
     wire.add(MessageType.LiquidUnblindOutput, __name__, "unblind_output", ns)
