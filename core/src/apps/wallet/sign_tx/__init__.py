d3192674d229709be8c774c633bdae9adf899bb3
Roman Zeyde <me@romanzey.de>
2019-07-12 14:49:28 +0300

    elements: support confidential transaction signing (PoC)
    Test by sending L-BTC to Elements' confidential addresses.

--- core/src/apps/wallet/sign_tx/__init__.py
+++ core/src/apps/wallet/sign_tx/__init__.py
@@ -1,4 +1,5 @@
 from trezor import utils, wire
+from trezor.crypto.curve import secp256k1_zkp  # TODO: not BITCOIN_ONLY
 from trezor.messages.RequestType import TXFINISHED
 from trezor.messages.TxAck import TxAck
 from trezor.messages.TxRequest import TxRequest
@@ -17,49 +18,50 @@ from apps.wallet.sign_tx import (
 
 
 async def sign_tx(ctx, msg, keychain):
-    signer = signing.sign_tx(msg, keychain)
+    with secp256k1_zkp.Context() as context:
+        signer = signing.sign_tx(msg, keychain, context)
 
-    res = None
-    while True:
-        try:
-            req = signer.send(res)
-        except (
-            signing.SigningError,
-            multisig.MultisigError,
-            addresses.AddressError,
-            scripts.ScriptsError,
-            segwit_bip143.Bip143Error,
-        ) as e:
-            raise wire.Error(*e.args)
-        if isinstance(req, TxRequest):
-            if req.request_type == TXFINISHED:
-                break
-            res = await ctx.call(req, TxAck)
-        elif isinstance(req, helpers.UiConfirmOutput):
-            mods = utils.unimport_begin()
-            res = await layout.confirm_output(ctx, req.output, req.coin)
-            utils.unimport_end(mods)
-            progress.report_init()
-        elif isinstance(req, helpers.UiConfirmTotal):
-            mods = utils.unimport_begin()
-            res = await layout.confirm_total(ctx, req.spending, req.fee, req.coin)
-            utils.unimport_end(mods)
-            progress.report_init()
-        elif isinstance(req, helpers.UiConfirmFeeOverThreshold):
-            mods = utils.unimport_begin()
-            res = await layout.confirm_feeoverthreshold(ctx, req.fee, req.coin)
-            utils.unimport_end(mods)
-            progress.report_init()
-        elif isinstance(req, helpers.UiConfirmNonDefaultLocktime):
-            mods = utils.unimport_begin()
-            res = await layout.confirm_nondefault_locktime(ctx, req.lock_time)
-            utils.unimport_end(mods)
-            progress.report_init()
-        elif isinstance(req, helpers.UiConfirmForeignAddress):
-            mods = utils.unimport_begin()
-            res = await paths.show_path_warning(ctx, req.address_n)
-            utils.unimport_end(mods)
-            progress.report_init()
-        else:
-            raise TypeError("Invalid signing instruction")
-    return req
+        res = None
+        while True:
+            try:
+                req = signer.send(res)
+            except (
+                signing.SigningError,
+                multisig.MultisigError,
+                addresses.AddressError,
+                scripts.ScriptsError,
+                segwit_bip143.Bip143Error,
+            ) as e:
+                raise wire.Error(*e.args)
+            if isinstance(req, TxRequest):
+                if req.request_type == TXFINISHED:
+                    break
+                res = await ctx.call(req, TxAck)
+            elif isinstance(req, helpers.UiConfirmOutput):
+                mods = utils.unimport_begin()
+                res = await layout.confirm_output(ctx, req.output, req.coin)
+                utils.unimport_end(mods)
+                progress.report_init()
+            elif isinstance(req, helpers.UiConfirmTotal):
+                mods = utils.unimport_begin()
+                res = await layout.confirm_total(ctx, req.spending, req.fee, req.coin)
+                utils.unimport_end(mods)
+                progress.report_init()
+            elif isinstance(req, helpers.UiConfirmFeeOverThreshold):
+                mods = utils.unimport_begin()
+                res = await layout.confirm_feeoverthreshold(ctx, req.fee, req.coin)
+                utils.unimport_end(mods)
+                progress.report_init()
+            elif isinstance(req, helpers.UiConfirmNonDefaultLocktime):
+                mods = utils.unimport_begin()
+                res = await layout.confirm_nondefault_locktime(ctx, req.lock_time)
+                utils.unimport_end(mods)
+                progress.report_init()
+            elif isinstance(req, helpers.UiConfirmForeignAddress):
+                mods = utils.unimport_begin()
+                res = await paths.show_path_warning(ctx, req.address_n)
+                utils.unimport_end(mods)
+                progress.report_init()
+            else:
+                raise TypeError("Invalid signing instruction")
+        return req
