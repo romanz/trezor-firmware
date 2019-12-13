d8225673ca58d7ff5db4d7931ee6faac089d869f
Roman Zeyde <me@romanzey.de>
2019-12-13 16:33:59 +0200

    elements: add rangeproof nonce derivation
    our_privkey = SLIP77(seed, script_pubkey)
    nonce = ECDH(our_privkey, peer_pubkey)

--- core/src/main.py
+++ core/src/main.py
@@ -34,6 +34,7 @@ def _boot_apps() -> None:
         import apps.eos
         import apps.binance
         import apps.webauthn
+        import apps.elements
 
     if __debug__:
         import apps.debug
@@ -54,6 +55,7 @@ def _boot_apps() -> None:
         apps.eos.boot()
         apps.binance.boot()
         apps.webauthn.boot()
+        apps.elements.boot()
     if __debug__:
         apps.debug.boot()
 
