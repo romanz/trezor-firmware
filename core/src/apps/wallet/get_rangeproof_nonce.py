78667a068f614e937191498fdac8f5cf09429c16
Roman Zeyde <me@romanzey.de>
2019-12-13 16:33:59 +0200

    elements: add rangeproof nonce derivation
    our_privkey = SLIP77(seed, script_pubkey)
    nonce = ECDH(our_privkey, peer_pubkey)

---
+++ core/src/apps/wallet/get_rangeproof_nonce.py
@@ -0,0 +1,14 @@
+from trezor.crypto.curve import secp256k1_zkp
+from trezor.crypto.hashlib import sha256
+from trezor.messages.ElementsRangeProofNonce import ElementsRangeProofNonce
+
+
+async def get_rangeproof_nonce(ctx, msg, keychain):
+    """Generate shared nonce using ECDH with our SLIP-77 private key and peer's public key."""
+    our_privkey = keychain.derive_slip77_blinding_private_key(msg.script_pubkey)
+    peer_pubkey = msg.ecdh_pubkey
+    with secp256k1_zkp.Context() as context:
+        compressed = True
+        shared_pubkey = context.multiply(our_privkey, peer_pubkey, compressed)
+    nonce = sha256(sha256(shared_pubkey).digest()).digest()
+    return ElementsRangeProofNonce(nonce=nonce)
